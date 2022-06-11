9d118510a77d212247f9ab045b209d4283e1a559
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-11 23:50:20 +0300

    feat: add base app

--- index.mjs
+++ index.mjs
@@ -1,1 +1,29 @@
-console.log('Welcome to the File Manager, Username!');
+import process, { argv } from 'process';
+import { createInterface } from 'readline';
+import { printGreetings, printFarewell, printPath } from './src/messages.mjs';
+import os from 'os';
+
+let currentDir = os.homedir();
+const readLine = createInterface({ input: process.stdin });
+
+printGreetings(argv);
+printPath(currentDir);
+
+function exit() {
+    printFarewell()
+    readLine.close()
+};
+
+readLine
+    .on('line', async (data) => {
+        if (data === '.exit' || data === '.Exit') {
+            exit()
+        }
+        else {
+            printPath(currentDir)
+            // console.log(`${data}`)
+        }
+    })
+
+    .on('SIGINT', () => { exit() })
+    .on('error', error => console.log('Error', error.message));
